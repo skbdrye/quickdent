7c50ee02ed3eb842e23b2474d3f3310e6327310b
Your Name <your-email@example.com>
2026-03-27 01:29:54 +0800

    Fix: Remove incompatible vite-plugin-enter-dev and fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
 import { defineConfig, PluginOption } from "vite";
-import { enterDevPlugin, enterProdPlugin } from 'vite-plugin-enter-dev';
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const plugins = [
-    ...enterProdPlugin(),
-  ];
-  if (mode === 'development') {
-    plugins.push(...enterDevPlugin());
-  }
+  const plugins: PluginOption[] = [];
   return {
     server: {
       host: "::",
